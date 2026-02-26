2f9fed0762358f3422f07945ce49ead6a8efd810
mjroldane <mjroldane@eafit.edu.co>
2026-02-26 13:27:37 -0500

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ st.title("mi primera app")
 st.header("Esta es mi pagina de presentación")
 
 image = Image.open("matildaRelajada.jpeg")
-st.image(image, caption:"matildaRelajada.jpeg")
+st.image(image, caption="matildaRelajada.jpeg")
 
 texto=st.text_input("ingresa texto", "texto inicial")
 st.write("el texto que haz escrito es",texto)
