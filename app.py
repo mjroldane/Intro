8d9c153511ff2dccdf5327d983e1ac81eaa7702f
mjroldane <mjroldane@eafit.edu.co>
2026-02-26 13:25:44 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 import streamlit as st
-import PIL import Image
+from PIL import Image
 st.title("mi primera app")
 st.header("Esta es mi pagina de presentación")
-image = Image.open(""matildaRelajada.jpeg)
-st.image(image, caption:"esta es mi gata matilda")
+
+image = Image.open("matildaRelajada.jpeg")
+st.image(image, caption:"matildaRelajada.jpeg")
+
+texto=st.text_input("ingresa texto", "texto inicial")
+st.write("el texto que haz escrito es",texto)
