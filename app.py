88796d03d1706479ec3c0273bf7d4eee334a4624
mjroldane <mjroldane@eafit.edu.co>
2026-02-26 13:22:05 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,3 +3,4 @@ import PIL import Image
 st.title("mi primera app")
 st.header("Esta es mi pagina de presentación")
 image = Image.open(""matildaRelajada.jpeg)
+st.image(image, caption:"esta es mi gata matilda")
